276f88262508260971be1cb4b06611edd66156ea
saparatayev <sopa.97@mail.ru>
2023-05-23 13:01:45 +0500

    twig looping

--- src/Controller/VinylController.php
+++ src/Controller/VinylController.php
@@ -2,10 +2,11 @@
 
 namespace App\Controller;
 
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class VinylController
+class VinylController extends AbstractController
 {
     #[Route('/')]
     public function homepage(): Response
@@ -17,5 +18,19 @@ class VinylController
     public function browse($slug): Response
     {
         return new Response('Title: ' . ($slug ?? 'no title'));
+    }
+
+    #[Route('/show-all')]
+    public function showAll(): Response
+    {
+        $tracks = [
+            ['title' => 'Anxiety', 'singer' => 'Marron 5'],
+            ['title' => 'Blind Lights', 'singer' => 'Weeknd'],
+            ['title' => 'We are the world', 'singer' => 'Michael Jackson'],
+        ];
+
+        return $this->render('vinyl/show_all.html.twig', [
+            'tracks' => $tracks,
+        ]);
     }
 }
